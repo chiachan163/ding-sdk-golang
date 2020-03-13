d91c2397afde0910a87331dbe635fe60bab891b7
chiachan <chiachan@163.com>
2020-03-13 16:11:56 +0800

    add: example/department

---
+++ example/department/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+
+	ding_sdk_golang "github.com/chiachan163/ding-sdk-golang"
+)
+
+func main() {
+	const CORPID = "xxx"
+	suiteAccessToken, err := ding_sdk_golang.GetSuiteToken("xxx", "xxx", "")
+	if err != nil {
+		log.Fatalf("ding_sdk_golang.GetSuiteToken error, err : %v", err)
+	}
+	log.Println("suite_access_token: ", suiteAccessToken)
+	accessToken, err := ding_sdk_golang.GetCorpToken(CORPID, suiteAccessToken)
+	if err != nil {
+		log.Fatalf("ding_sdk_golang.GetCorpToken error, err : %v", err)
+	}
+	log.Println("access_token: ", accessToken)
+
+	//// 获取部门详情
+	//department, err := ding_sdk_golang.GetDepartment(accessToken, 1, nil)
+	//if err != nil {
+	//	log.Fatalf("ding_sdk_golang.GetDepartment error, err : %v", err)
+	//}
+	//log.Println(*department)
+
+	//// 获取部门列表
+	//var id int64 = 1
+	//department, err := ding_sdk_golang.DepartmentList(accessToken, &id, nil, nil)
+	//if err != nil {
+	//	log.Fatalf("ding_sdk_golang.GetDepartment error, err : %v", err)
+	//}
+	//log.Println(department[0])
+
+	//// 获取子部门ID列表
+	//subDeptIdList, err := ding_sdk_golang.DepartmentListIds(accessToken, 1)
+	//if err != nil {
+	//	log.Fatalf("ding_sdk_golang.GetDepartment error, err : %v", err)
+	//}
+	//log.Println(subDeptIdList)
+
+	//// 查询指定用户的所有上级父部门路径
+	//department, err := ding_sdk_golang.DepartmentListParentDepts(accessToken, "manager2872")
+	//if err != nil {
+	//	log.Fatalf("ding_sdk_golang.GetDepartment error, err : %v", err)
+	//}
+	//log.Println(department)
+
+	// 查询部门的所有上级父部门路径
+	parentIds, err := ding_sdk_golang.DepartmentListParentDeptsByDept(accessToken, 280467091)
+	if err != nil {
+		log.Fatalf("ding_sdk_golang.GetDepartment error, err : %v", err)
+	}
+	log.Println(parentIds)
+
+	//log.Println("ticket: ", fmt.Sprintf("%p", unsafe.Pointer(ticket)))
+}
